5d2283dc536e598216faeb68b5fcfd23946b24b1
Piotr Berłowski <p.berlowski@kolejewlkp.pl>
2020-12-05 22:31:53 +0100

    one next step table & form exchange data begins

--- public/js/DataGrid.js
+++ public/js/DataGrid.js
@@ -1,111 +1,174 @@
-class DataGrid {
 
-    tableHeader;
-    tableBody;
+class
+DataGrid
+{
+
+    eTableHeader;
+    eTableBody;
     dataSourceConfig;
     dataSource;
-    result;
-    form;
+    eForm;
+    eContainer;
 
-    onEnter() {
-        let form = document.getElementById("form");
-        let container = document.getElementById("table");
-        form.style.display = "block";
-        form.width = container.width;
-        form.height = container.height;
-        form.top = "0px"
+
+    static setEditFormData(element)
+    {
+        let row = element.parentNode;
+        let dataRow = row.data;
+        return dataRow;
+    }
+
+    static setupRow(rowData)
+    {
+        let row = rowData[0];
+        let rowNumber = rowData[1];
+        let fieldsData = rowData[2];
 
     }
 
+    displayForm()
+    {
+        window.dgTable.style.display = "none";
+        let form = window.dgForm;
+        form.style.display = "block";
+        let dataRow = DataGrid.setEditFormData(this);
+        form.data = dataRow;
+        dataRow = dataRow[2];
 
-    dataGridHeader() {
-        this.tableHeader.className = headerDefinition.headerClass;
+        var element = form.firstChild;
+        while (element.tagName != "BUTTON") {
+
+            if (element.tagName == "INPUT") {
+                element.value = dataRow[element.name];
+            }
+            element = element.nextSibling;
+        }
+        DataGrid.setupRow(dataRow);
+    }
+
+    displayGrid()
+    {
+        window.dgTable.style.display = "table";
+        let form = window.dgForm;
+        let element = form.firstChild;
+        let formData = form.data;
+        let dataRow = formData[1, 1];
+
+        while (element.tagName != "BUTTON") {
+
+            if (element.tagName == "INPUT") {
+                dataRow[element.name] = element.value;
+            }
+            element = element.nextSibling;
+        }
+
+    }
+
+    dataGridHeader()
+    {
+        this.eTableHeader.className = headerDefinition.headerClass;
         let header = headerDefinition.headerCells;
         var i;
         for (i = 0; i < header.length; i++) {
             let cell = document.createElement("th")
-            this.tableHeader.appendChild(cell);
+            this.eTableHeader.appendChild(cell);
             cell.className = header[i].cellClass;
             cell.innerHTML = header[i].label;
         }
     }
 
-    editForm() {
 
-        this.form = document.createElement("div");
-        this.form.className = this.headerDefinition.formClassName;
-        this.container.appendChild(this.form);
-        this.form.style.display = "none";
-        this.form.id = "form";
+    createEditForm()
+    {
+
+        this.eForm.style.display = "none";
+        this.eForm.id = "dgForm";
+        this.eForm.style.minHeight = headerDefinition.maxHeight;
+        this.eForm.width = this.eContainer.width;
         let header = headerDefinition.headerCells;
         var i;
         for (i = 0; i < header.length; i++) {
+
+            let input = document.createElement("input");
+            input.type = header[i].inputType;
+            input.className = header[i].inputClass;
             if (header[i].inputType != "hidden") {
                 let label = document.createElement("label")
                 label.innerHTML = header[i].label;
-                this.form.appendChild(label);
-                let input = document.createElement("input");
-                input.type = header[i].inputType;
-                this.form.appendChild(input);
+                this.eForm.appendChild(label);
             }
+            input.name = header[i].fieldName;
+            this.eForm.appendChild(input);
+            this.eForm.appendChild(input);
         }
+
+        let button = document.createElement("button");
+        button.inputType = "button";
+        button.className = headerDefinition.buttonClass;
+        this.eForm.appendChild(button);
+        button.addEventListener("click", this.displayGrid);
+        button.innerHTML = "Save";
     }
 
 
-    dataGridRow(rowNumber, rowData) {
+    dataGridRow(rowNumber, rowData)
+    {
 
         let fields = rowDefinition.fields;
         let rowClass = rowDefinition.rowClassName;
 
         let tr = document.createElement("tr");
         tr.className = rowClass;
-        tr.data = rowData;
-        this.tableBody.appendChild(tr);
+        tr.data = [tr,rowNumber, rowData];
+        this.eTableBody.appendChild(tr);
         var i;
         for (i = 0; i < fields.length; i++) {
             let td = document.createElement("td");
             td.innerHTML = rowData[fields[i].name];
             td.className = fields[i].fieldClass;
-            td.addEventListener("click", this.onEnter);
+            td.addEventListener("click", this.displayForm);
             tr.appendChild(td)
         }
     }
 
 
-    constructor(container, className, name, headerDefinition, rowDefinition, rowCount) {
+    constructor(container, className, name, headerDefinition, rowDefinition, rowCount)
+    {
 
-        this.container = container;
+        this.eContainer = container;
         this.className = className;
         this.headerDefinition = headerDefinition;
         this.rowDefinition = rowDefinition;
         this.rowCount = rowCount;
         this.name = name;
 
-        this.container.style.maxHeight = "360px"
-        this.container.style.overflow = "auto"
+        this.eContainer.style.maxHeight = this.headerDefinition.maxHeight;
+        this.eContainer.style.overflow = "auto"
 
-        let colCount = this.headerDefinition.headerCells.length;
-        let table = document.createElement("table");
-        table.className = this.className;
-        table.id = this.name;
-        this.container.appendChild(table);
+        this.eTable = document.createElement("table");
+        this.eTable.className = this.className;
+        this.eTable.id = "dgTable";
+        this.eContainer.appendChild(this.eTable);
 
-        this.tableHeader = document.createElement("tr");
-        table.appendChild(this.tableHeader)
-
+        this.eTableHeader = document.createElement("tr");
+        this.eTable.appendChild(this.eTableHeader)
         this.dataGridHeader();
-        this.tableBody = document.createElement("tbody");
-        table.appendChild(this.tableBody)
 
-        this.result = document.getElementById("result");
-        this.editForm();
+        this.eTableBody = document.createElement("tbody");
+        this.eTable.appendChild(this.eTableBody)
+
+        this.eForm = document.createElement("div");
+        this.eForm.className = this.headerDefinition.formClassName;
+        this.eContainer.appendChild(this.eForm);
+        this.createEditForm();
     }
 
-    build() {
+    build()
+    {
         var i;
         for (i = 0; i < this.rowCount; i++) {
             let rowData = this.dataSource.getRowData(i);
-            this.dataGridRow(i, rowData)
+            this.dataGridRow(i, rowData);
         }
     }
 
--- public/js/start.js
+++ public/js/start.js
@@ -1,23 +1,33 @@
 var headerDefinition = {
-    "formClassName":"w3-card",
-    "headerClass":"w3-theme",
+    "headerClass": "w3-theme",
+    "maxHeight":"360px",
     "headerCells": [
         {
             "cellClass": "w3-right-align w3-border",
             "label": "id",
-            "inputType":"hidden"
+            "inputType": "hidden",
+            "inputClass":"w3-input",
+            "fieldName":"id"
+
         },
         {
             "cellClass": "w3-center  w3-border",
             "label": "First name",
-            "inputType":"text"
+            "inputType": "text",
+            "inputClass":"w3-input w3-margin",
+            "fieldName":"fname"
         },
         {
             "cellClass": "w3-center w3-border",
             "label": "Last name",
-            "inputType":"text"
+            "inputType": "text",
+            "inputClass":"w3-input w3-margin",
+            "fieldName":"lname"
+
         }
-    ]
+    ],
+    "formClassName": "w3-container w3-margin w3-padding-large",
+    "buttonClass":"w3-button w3-theme"
 }
 
 var rowDefinition = {
@@ -43,5 +53,4 @@ function start(container, rowcount, dataSourceConfig) {
     this.dataSourceConfig = dataSourceConfig
     var grid = new DataGrid(container, "w3-table-all w3-hoverable", "dataGrid", headerDefinition, rowDefinition, 10);
     let dataSource = new DataSource(dataSourceConfig, grid);
-
 }
